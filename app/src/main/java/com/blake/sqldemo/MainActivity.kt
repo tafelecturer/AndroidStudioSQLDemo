d22fbb83dc65160a7fbb103f32c3b0d9c699745a
smashinu <smashinu2@gmail.com>
2025-08-26 17:44:49 +0800

    Added Dialogs to android Studios

--- app/src/main/java/com/blake/sqldemo/MainActivity.kt
+++ app/src/main/java/com/blake/sqldemo/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.blake.sqldemo
 
+import android.content.DialogInterface
 import android.os.Bundle
 import android.service.autofill.Validators.or
 import android.widget.Button
@@ -7,6 +8,7 @@ import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -19,16 +21,33 @@ class MainActivity : AppCompatActivity() {
         val btnAddUser = findViewById<Button>(R.id.btnAddUser)
 
         btnAddUser.setOnClickListener {
-            val db = DBHelper(this, null)
-            val etUserName = findViewById<EditText>(R.id.etUserName)
-            val etAge = findViewById<EditText>(R.id.etAge)
-            val name = etUserName.text.toString()
-            val age = etAge.text.toString()
-            db.addUser(name, age)
-            // Toast to message on the screen
-            Toast.makeText(this, name + " added to database", Toast.LENGTH_SHORT).show()
-            etUserName.text.clear()
-            etAge.text.clear()
+            val dialogBuilder = AlertDialog.Builder(this)
+            val inflater = this.layoutInflater
+            val dialogView = inflater.inflate(R.layout.add_dialog, null)
+            dialogBuilder.setTitle("Add New User")
+            dialogBuilder.setMessage("Enter data below")
+            dialogBuilder.setView(dialogView)
+            // Set a listener to each button that takes an action before dismissing the dialog
+            // The dialog is automatically dismissed when a dialog button is clicked
+            dialogBuilder.setPositiveButton("Yes",
+                DialogInterface.OnClickListener { dialog, id ->
+                    // do this if "Yes" is clicked
+                    val db = DBHelper(this, null)
+                    val etAddName = dialogView.findViewById<EditText>(R.id.etAddName)
+                    val etAddAge = dialogView.findViewById<EditText>(R.id.etAddAge)
+                    val name = etAddName.text.toString()
+                    val age = etAddAge.text.toString()
+                    db.addUser(name, age)
+                    Toast.makeText(this, "$name added to database", Toast.LENGTH_SHORT).show()
+                }
+            )
+            dialogBuilder.setNegativeButton("No",
+                DialogInterface.OnClickListener { dialog, id ->
+                    // do this if "No" is clicked
+                    // Nothing is performed, so you can put null instead
+                })
+            dialogBuilder.setIcon(android.R.drawable.ic_dialog_alert)
+            dialogBuilder.show()
         }
 
         val btnPrintUsers = findViewById<Button>(R.id.btnPrintUsers)
@@ -42,28 +61,64 @@ class MainActivity : AppCompatActivity() {
 
         val btnDeleteUser = findViewById<Button>(R.id.btnDeleteUser)
         btnDeleteUser.setOnClickListener {
-            val db = DBHelper(this, null)
-            val userName = findViewById<EditText>(R.id.etUserName).text.toString()
-            val rows = db.deleteUser(userName)
-            Toast.makeText(
-                this,
-                when (rows) {
-                    0 -> "Nothing deleted"
-                    1 -> "1 user deleted"
-                    else -> "" // shouldn't happen
-                },
-                Toast.LENGTH_LONG
-            ).show()
+            val dialogBuilder = AlertDialog.Builder(this)
+            val dialogView = this.layoutInflater.inflate(R.layout.delete_dialog, null)
+            dialogBuilder.setTitle("Delete User")
+            dialogBuilder.setMessage("Enter data below")
+            dialogBuilder.setView(dialogView)
+            // Set a listener to each button that takes an action before dismissing the dialog
+            // The dialog is automatically dismissed when a dialog button is clicked
+            dialogBuilder.setPositiveButton("Yes",
+                DialogInterface.OnClickListener { dialog, id ->
+                    // do this if "Yes" is clicked
+                    val db = DBHelper(this, null)
+                    val etDeleteName = dialogView.findViewById<EditText>(R.id.etDeleteName)
+                    val name = etDeleteName.text.toString()
+                    val rows = db.deleteUser(name)
+                    Toast.makeText(
+                        this,
+                        when (rows) {
+                            0 -> "Nothing deleted"
+                            1 -> "1 user deleted"
+                            else -> "" // shouldn't happen
+                        },
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            )
+            dialogBuilder.setNegativeButton("No", null) // nothing to do
+            dialogBuilder.setIcon(android.R.drawable.ic_dialog_alert)
+            dialogBuilder.show()
         }
 
         val btnUpdateUser = findViewById<Button>(R.id.btnUpdateUser)
         btnUpdateUser.setOnClickListener {
-
-            val db = DBHelper(this, null)
-            val userName = findViewById<EditText>(R.id.etUserName).text.toString()
-            val age = findViewById<EditText>(R.id.etAge).text.toString()
-            val rows = db.updateUser(userName, age)
-            Toast.makeText(this, "$rows users updated", Toast.LENGTH_LONG).show()
+            val dialogView = this.layoutInflater.inflate(R.layout.update_dialog, null)
+            /*
+            Kotlin is both a functional programming and an OOP language
+            */
+            AlertDialog.Builder(this)
+                .setTitle("Update User")
+                .setMessage("Enter data below")
+                .setView(dialogView)
+                // Set a listener to each button that takes an action before dismissing the dialog
+                // The dialog is automatically dismissed when a dialog button is clicked
+                .setPositiveButton(
+                    "Yes",
+            DialogInterface.OnClickListener { dialog, id ->
+                // do this if "Yes" is clicked
+                val db = DBHelper(this, null)
+                val etUpdateName = dialogView.findViewById<EditText>(R.id.etUpdateName)
+                val etUpdateAge = dialogView.findViewById<EditText>(R.id.etUpdateAge)
+                val name = etUpdateName.text.toString()
+                val age = etUpdateAge.text.toString()
+                val rows = db.updateUser(name, age)
+                Toast.makeText(this, "$rows users updated", Toast.LENGTH_LONG).show()
+            }
+            )
+            .setNegativeButton("No", null) // nothing to do
+            .setIcon(android.R.drawable.ic_dialog_alert)
+            .show()
         }
 
         val btnDeleteDB = findViewById<Button>(R.id.btnDeleteDB)
